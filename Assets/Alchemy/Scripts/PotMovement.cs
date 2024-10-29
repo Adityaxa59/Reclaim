d944fe4b78756de929506adb710d9b46c2233179
Dark-Sou1 <Aditya>
2024-10-29 22:30:14 +0400

    Added Pot Movement Script

--- Assets/Alchemy/Scripts/PotMovement.cs
+++ Assets/Alchemy/Scripts/PotMovement.cs
@@ -1,16 +1,37 @@
 using UnityEngine;
 
-public class PotMovement : MonoBehaviour
+namespace Alchemy.Scripts
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class PotMovement : MonoBehaviour
     {
-        
-    }
+        [SerializeField] float maxY = 7;
+        private Vector3 _rawMousePosition;
+        private Vector3 _translatedPosition;
+        private Camera _camera;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private void Start()
+        {
+            _camera = Camera.main;
+        }
+
+        private void Update()
+        {
+            if (Input.GetMouseButton(0)) //on drag
+            {
+                _rawMousePosition = Input.mousePosition;
+                // ReSharper disable once PossibleNullReferenceException
+                _translatedPosition = _camera.ScreenToWorldPoint(_rawMousePosition); //converts screen position of mouse to game world position
+                
+                if (_translatedPosition.x > maxY) // screen boundary right
+                {
+                    _translatedPosition.x = maxY;
+                }
+                else if (_translatedPosition.x < -maxY) // screen boundary left
+                {
+                    _translatedPosition.x = -maxY;
+                }
+                transform.position = new Vector3(_translatedPosition.x, transform.position.y, transform.position.z);// moves object. vector z becomes -10(pos of camera) which hides the object
+            }
+        }
     }
 }
